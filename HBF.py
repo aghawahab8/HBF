2c9ad2fe9d73e8685c03c9b9ae94304df66bf05c
HAMID MEER <85035495+Hamii-king-06@users.noreply.github.com>
2022-06-14 22:03:27 +0500

    Create HBF.py

---
+++ HBF.py
@@ -0,0 +1,21 @@
+import os, platform
+
+os.system('git pull')
+
+ 
+
+import requests
+
+bit = platform.architecture()[0]
+
+if bit == '64bit':
+
+    from HBF64 import menu
+
+    hbf()
+
+elif bit == '32bit':
+
+    from HBF import menu
+
+    hbf()
